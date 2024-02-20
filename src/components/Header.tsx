5f56154f04f87ec06636dd76537368b2b414d6ce
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2024-02-20 12:17:53 +0200

    Created functional navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image"
 import { Split, Wrapper } from "../layouts"
+import { Navbar } from "./index"
+import type { ILink } from "../types"
+
+const links: ILink[] = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Reviews", href: "/reviews" },
+  { name: "Author", href: "https://www.andreyperunov.com/" }
+]
 
 export default function Header() {
   return (
@@ -10,7 +19,7 @@ export default function Header() {
             <Image className="w-9 h-9 mr-3" src="/logo.png" width={40} height={40} alt="Logo" />
             <span className="font-bold text-white text-sm">TSI Schedule Scraper</span>
           </div>
-          <div className="flex items-center">test test</div>
+          <Navbar links={links} />
         </Split>
       </Wrapper>
     </header>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,18 @@
+"use client"
 import type { ILink } from "../types"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Navbar({ links }: { links: ILink[] }) {
+  const pathname = usePathname()
   return (
     <div className="flex items-center">
       {links.map(link => {
+        const isActive = pathname.startsWith(link.href)
         return (
-          <a key={link.href} href={link.href}>
+          <Link key={link.href} href={link.href} className={`text-sm text-white pl-7 ${isActive && "font-bold underline"}`}>
             {link.name}
-          </a>
+          </Link>
         )
       })}
     </div>
