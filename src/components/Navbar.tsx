feeb3c87eed08549d24bdfc31f4cd6db476abf9e
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2024-02-21 12:25:25 +0200

    Changed component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,13 @@ import type { ILink } from "../types"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
-export default function Navbar({ links }: { links: ILink[] }) {
+export default function Navbar({ links, className, ...props }: { links: ILink[]; className?: string }) {
   const pathname = usePathname()
   return links.map(link => {
     const isActive = pathname.startsWith(link.href)
     return (
       <>
-        <Link key={link.href} href={link.href} className={`text-sm text-white pl-7 active:text-dark hover:underline active ${isActive && "font-bold"}`}>
+        <Link key={link.href} href={link.href} className={`text-sm text-white pl-7 active:text-dark hover:underline active ${isActive && "font-bold"} ${className}`}>
           {link.name}
         </Link>
       </>
