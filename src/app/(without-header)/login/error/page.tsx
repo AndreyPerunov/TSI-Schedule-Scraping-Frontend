79479ccf2f66515a3ef1fc3d96444745e5c46366
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2024-05-24 18:04:36 +0300

    Added error messages

--- src/app/(without-header)/login/error/page.tsx
+++ src/app/(without-header)/login/error/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link"
 import Image from "next/image"
 import { FaRegCircleXmark } from "react-icons/fa6"
 
-export default function Fail() {
+export default function Fail({ searchParams }: { searchParams: URLSearchParams }) {
+  const { error_message } = searchParams as any
   return (
     <main className="bg-gradient-to-r from-primary to-secondary h-full min-h-screen flex justify-center items-start">
       <div className="mt-24 min-w-96 ">
@@ -12,7 +13,7 @@ export default function Fail() {
         <div className="bg-white mt-2.5 py-10 px-16 rounded-lg flex flex-col items-center">
           <Image src="/logo.png" width={300} height={300} priority={true} alt="Logo" className="w-28" />
           <h1 className="mt-10 mb-0 text-2xl font-bold text-primary">Error!</h1>
-          <p className="mt-2 text-passive font-semibold text-center">Something went wrong. Try again later.</p>
+          <p className="mt-2 text-passive font-semibold text-center">{error_message ? error_message : "Something went wrong. Try again later."}</p>
           <FaRegCircleXmark className="mt-10 text-red-400" size={80} />
         </div>
       </div>
