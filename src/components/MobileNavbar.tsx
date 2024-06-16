7b7daf9be3373287defcc999bed468072e33f8bd
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2024-06-17 01:15:33 +0300

    Added link to profile

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -6,10 +6,12 @@ import Link from "next/link"
 import { FaExternalLinkAlt } from "react-icons/fa"
 import LinkButton from "./LinkButton"
 import { useAppSelector } from "@/hooks"
+import Image from "next/image"
+import { IoMdSettings } from "react-icons/io"
 
 export default function MobileNavbar({ links, className, ...props }: { links: ILink[]; className?: string }) {
   const [open, setOpen] = useState(false)
-  const { isAuth } = useAppSelector(state => state.authReducer)
+  const { isAuth, googleName, googlePicture } = useAppSelector(state => state.authReducer)
 
   return (
     <div className={`${className}`} {...props} onClick={() => setOpen(true)}>
@@ -23,6 +25,16 @@ export default function MobileNavbar({ links, className, ...props }: { links: IL
           <div className="flex flex-col">
             {isAuth ? (
               <>
+                <div className="flex items-center">
+                  <span className="font-semibold items-center text-white ml-5">Welcome, {googleName || "User"}!</span>
+
+                  <Link href="/profile" className="text-sm ml-2 active:text-dark hover:underline active">
+                    <div className="bg-white bg-opacity-0 rounded-full size-11 flex justify-center items-center transition-all duration-200 hover:rotate-180 hover:bg-opacity-20">
+                      <IoMdSettings className="inline text-white text-2xl" />
+                    </div>
+                  </Link>
+                  <Image src={googlePicture} width={50} height={50} alt="Profile" className="ml-2 rounded-full size-11" />
+                </div>
                 <LinkButton href="/logout" className="mb-4 mt-4" hollow={true}>
                   Log Out
                 </LinkButton>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,27 +2,39 @@
 import type { ILink } from "../types"
 import Link from "next/link"
 import { FaExternalLinkAlt } from "react-icons/fa"
+import { IoMdSettings } from "react-icons/io"
+import Image from "next/image"
 import { HeaderButton } from "@/components"
 import { useAppSelector } from "@/hooks"
 
 export default function Navbar({ links, className, ...props }: { links: ILink[]; className?: string }) {
-  const { isAuth } = useAppSelector(state => state.authReducer)
+  const { isAuth, googleName, googlePicture } = useAppSelector(state => state.authReducer)
 
   return (
     <nav className={className} {...props}>
       {links.map(link => {
         return (
-          <Link key={link.href} href={link.href} className="flex justify-center	items-center text-sm text-white ml-7 active:text-dark hover:underline active">
+          <Link key={link.href} href={link.href} className="flex justify-center items-center text-sm text-white ml-7 active:text-dark hover:underline active">
             {link.name}
             {link.external && <FaExternalLinkAlt className="inline ml-1" />}
           </Link>
         )
       })}
       {isAuth ? (
-        <HeaderButton href={["/logout", "/dashboard"]}>
-          <span>Log Out</span>
-          <span>Dashboard</span>
-        </HeaderButton>
+        <>
+          <HeaderButton href={["/logout", "/dashboard"]}>
+            <span>Log Out</span>
+            <span>Dashboard</span>
+          </HeaderButton>
+          <span className="flex justify-center font-semibold items-center text-white ml-5">Welcome, {googleName || "User"}!</span>
+
+          <Link href="/profile" className="flex justify-center items-center text-sm ml-2 active:text-dark hover:underline active">
+            <div className="bg-white bg-opacity-0 rounded-full size-11 flex justify-center items-center transition-all duration-200 hover:rotate-180 hover:bg-opacity-20">
+              <IoMdSettings className="inline text-white text-2xl" />
+            </div>
+          </Link>
+          <Image src={googlePicture} width={50} height={50} alt="Profile" className="ml-2 rounded-full size-11" />
+        </>
       ) : (
         <HeaderButton href={["/login", "/signup"]}>
           <span>Sign In</span>
